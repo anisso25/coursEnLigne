df9cbe8a7392b1fd425606d0b9700084e70cd655
Mr.Robot <macbookpro@MacBook-Pro-de-MacBook.local>
2023-07-03 22:18:15 +0100

    first commit 2

--- resources/js/Pages/Courses/Index.vue
+++ resources/js/Pages/Courses/Index.vue
@@ -1,6 +1,9 @@
 <template>
     <app-layout>
-        <h1>Coucou</h1>
+        <template v-slot="header">
+            Liste des formation
+            <h1>CouCou</h1>
+        </template>
     </app-layout>
 </template>
 
