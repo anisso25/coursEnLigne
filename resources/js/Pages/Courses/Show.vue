03c9c01d0eb49ce351d9c74a92be0506f6363cb7
Mr.Robot <macbookpro@MacBook-Pro-de-MacBook.local>
2023-07-05 21:34:39 +0100

    interface episodes 1

--- resources/js/Pages/Courses/Show.vue
+++ resources/js/Pages/Courses/Show.vue
@@ -5,6 +5,7 @@
         </template>
         <div class="py-4 mx-8">
             <div class="text-2xl">{{ course.episodes[0].title }}</div>
+            
             <!-- <iframe class="w-full h-screen" src="https://www.dailymotion.com/embed/video/x8m9hym?autoplay=1" 
             width="100%" height="100%" allowfullscreen title="Dailymotion Video Player" allow="autoplay"> </iframe> -->
             
@@ -18,7 +19,14 @@
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen></iframe>
             
-            <div class="text-sm text-gray-500">{{ course.episodes[0].description }}</div>
+            <div class="py-3 text-sm text-gray-500">{{ course.episodes[0].description }}</div>
+
+            <div class="mt-8">
+                <ul v-for="(episode, index) in this.course.episodes" v-bind:key="episode.id">
+                    <li class="mt-3" >Episode n°{{ index + 1 }} - {{ episode.title }}</li>
+                    <button class="text-gray-500 focus:text-indigo-500 focus:outline-none" @click="switchEpisode()">voir l'épisode</button>
+                </ul>
+            </div>
         </div>
     </app-layout>
 </template>
